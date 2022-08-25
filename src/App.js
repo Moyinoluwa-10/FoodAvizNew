f4b9eb858ad8fc63e9a15aa583c99ccbe3bc2ed4
AlmeenT <100124797+AlmeenT@users.noreply.github.com>
2022-08-24 21:17:16 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import SignUp from "./Component/SignUp";
 import SignIn from "./Component/SignIn";
 import ScrollToTop from "./ScrollToTop";
-import Onboarding1 from "./Component/Onboarding1";
-import Onboarding2 from "./Component/Onboarding2";
-import Onboarding3 from "./Component/Onboarding3";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 
@@ -12,9 +9,6 @@ function App() {
     <Router>
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Onboarding1 />} />
-        <Route path="/onboarding2" element={<Onboarding2 />} />
-        <Route path="/onboarding3" element={<Onboarding3 />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dash" element={<Dashboard />} />
