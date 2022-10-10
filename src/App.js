dd6afd7dec3784c2ada94c3341447df573e222d5
Moyinoluwa-10 <moyinadelowo@gmail.com>
2022-10-11 00:36:20 +0100

    Built the landing page fully with no responsiveness

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import SignUp from "./Component/SignUp";
-import SignIn from "./Component/SignIn";
+import SignUp from "./Pages/SignUp/SignUp";
+import SignIn from "./Pages/SignIn/SignIn";
 import ScrollToTop from "./ScrollToTop";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import Landing from "./Pages/LandingPage/Landing";
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
       <Routes>
+        <Route exact path="/" element={<Landing />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dash" element={<Dashboard />} />
